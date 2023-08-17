118df93598fcd361a07b8ac7e2c570197672ec1f
Dacaramo <dankramirez@outlook.com>
2023-08-17 12:39:18 -0500

    style: fixed some eslint warnings

--- src/zustand/store.ts
+++ src/zustand/store.ts
@@ -1,13 +1,13 @@
 import { CanvasSlice, createCanvasSlice } from './slices/canvasSlice';
-import { create } from 'zustand';
 import { ToolbarSlice, createToolbarSlice } from './slices/toolbarSlice';
+import { create } from 'zustand';
 
 /**
  * A type that joins all of the slices defined by the programmers
  */
 export type StoreState = ToolbarSlice & CanvasSlice;
 
-const useStore = create<StoreState>((...args) => {
+export const useStore = create<StoreState>((...args) => {
   return {
     ...createCanvasSlice(...args),
     ...createToolbarSlice(...args),
